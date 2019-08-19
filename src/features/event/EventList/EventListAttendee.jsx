54a860e128de1cb53a38f7c32ba5aafa1f80d343
Vishesh Sanghani <vishesh.sanghani95@gmail.com>
2019-08-19 16:55:41 +0530

    commit  (3)

---
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { List, Image } from 'semantic-ui-react';
+
+class EventListAttendee extends Component {
+    render() {
+        return (
+            <List.Item>
+                <Image as = "A" size = "mini" circular src={this.props.attendee.photoURL} />
+            </List.Item>
+        )
+    }
+}
+
+export default EventListAttendee;
---
+++ src/features/event/EventList/EventListItem.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Segment , List , Item , Button , Icon } from "semantic-ui-react";
+import EventListAttendee from './EventListAttendee';
+
+class EventListItem extends Component {
+    render() {
+        return (
+            <div>
+                     <Segment.Group>
+                        <Segment>
+                          <Item.Group>
+                            <Item>
+                              <Item.Image size="tiny" circular src={this.props.event.hostPhotoURL} />
+                              <Item.Content>
+                                <Item.Header as="a">{this.props.event.title}</Item.Header>
+                                <Item.Description>
+                                  Hosted by <a>{this.props.event.hostedBy}</a>
+                                </Item.Description>
+                              </Item.Content>
+                            </Item>
+                          </Item.Group>
+                        </Segment>
+                        <Segment>
+                          <span>
+                            <Icon name="clock" /> {this.props.event.date} |
+                            <Icon name="marker" /> {this.props.event.venue}
+                          </span>
+                        </Segment>
+                        <Segment secondary>
+                          <List horizontal>
+                          {this.props.event.attendees.map(attendee =>(
+                            <EventListAttendee attendee = {attendee} key={attendee.id}  />
+                          ))}
+                          </List>
+                        </Segment>
+                        <Segment clearing>
+                          <span>{this.props.event.description}</span>
+                          <Button as="a" color="teal" floated="right" content="View" />
+                        </Segment>
+                      </Segment.Group>
+            </div>
+        )
+    }
+}
+
+export default  EventListItem;
